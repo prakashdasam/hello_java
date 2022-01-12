2e481c27e00cfd4024bcf7f8bff436f0e1c3196c
jmstechhome14 <jmstechhome14@gmail.com>
2022-01-12 16:19:49 +0000

    adding hello world class multiple line

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,15 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
+    System.out.println("Hello, World");
   }
 }
