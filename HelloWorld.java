3b3162d75b5a507ff4621834929d7727c0730103
jmstechhome13 <96036369+jmstechhome13@users.noreply.github.com>
2022-01-18 21:29:22 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Happy sankranti to all... enjoy holidays next meet will be tuesday...");
+    System.out.println("Happy republic day");
   }
 }
